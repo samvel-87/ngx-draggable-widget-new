4d1050e045a33f5dd430a30b9fc9fa8b78c0a3cb
Samvel Asatryan <samvelasatryan@intelinair.com>
2024-07-25 16:30:23 +0400

    Install angular 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NgDraggableWidgetModule} from 'ngx-draggable-widget-kama';
+import {
+  NgDraggableWidgetModule
+} from '../../projects/ngx-draggable-widget-kama/src/lib/ngx-draggable-widget-kama.module';
 
 @NgModule({
   declarations: [
